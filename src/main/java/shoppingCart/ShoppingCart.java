51cc34a8091761e94d9762797675e8a38638b4a4
iDaryna <dargud99@gmail.com>
2024-10-22 13:14:56 -0400

    created ShoppingCart fields

---
+++ src/main/java/shoppingCart/ShoppingCart.java
@@ -0,0 +1,10 @@
+package shoppingCart;
+
+import java.util.ArrayList;
+
+public class ShoppingCart {
+    private ArrayList<MenuItems> items;
+    private double totalPrice;
+    private int itemsInCart;
+
+}
